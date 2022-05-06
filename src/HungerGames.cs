0ca7edcc925aeac14dbf8571e66926654bd4ecf7
treeman0013 <foregoneocean89@gmail.com>
2022-05-05 18:55:12 -0600

    initial commit

---
+++ src/HungerGames.cs
@@ -0,0 +1,38 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo( "HungerGames", Description = "Hello World", Version = "0.1.0", Website = "website", Authors = new []{ "treeman0013" } )]
+
+namespace HungerGames
+{
+	public class HungerGames : ModSystem
+	{
+		ICoreClientAPI capi;
+		HudElement hud;
+
+		public override bool ShouldLoad(EnumAppSide forSide)
+		{
+			return forSide == EnumAppSide.Client;
+		}
+			
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			base.StartClientSide(api);
+
+			this.capi = api;
+			capi.Input.RegisterHotKey("annoyingtextgui", "Annoys you with annoyingly centered text", GlKeys.U, HotkeyType.GUIOrOtherControls);
+			capi.Input.SetHotKeyHandler("annoyingtextgui", ToggleGui);
+
+			hud = new GuiPlayerCount(capi);
+		}
+
+		private bool ToggleGui(KeyCombination comb)
+		{
+			if (hud.IsOpened()) hud.TryClose();
+			else hud.TryOpen();
+
+			return true;
+		}
+	}
+}
